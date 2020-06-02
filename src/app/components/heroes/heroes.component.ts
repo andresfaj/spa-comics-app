61b6579c9d480037b8613f3c2711dec91fa9ad17
andresfaj <achuryjaramillo@gmail.com>
2020-06-01 22:57:52 -0500

    routes with parameters, ej: heroe/:id

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +10,18 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService) {
-    console.log('constructor');
+  constructor(private _heroesService: HeroesService,
+              private router: Router        ) {
+    // console.log('constructor');
   }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes);
+    // console.log(this.heroes);
+  }
+
+  seeHeroe( id: number) {
+    this.router.navigate(['/heroe', id]);
   }
 
 }
